1f662e897b5460eca8c4479d9c82b1649e3e2ecc
silvercrow31 <53498833+silvercrow31@users.noreply.github.com>
2026-05-19 19:46:24 -0400

    Move to correct folder

--- DuocUC/19 - while True y try/except/02_try_except.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        edad = int(input("Ingrese edad: "))
-
-        print(f"La edad es {edad}, y es válida")
-    except ValueError:
-        print("\n Error: debe ser un numero!!")
